1d551093ccc65ad388a02589df4bf1fe4dcabc35
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-02 10:53:44 +0530

    very easy

--- InterviewBit/ReverseBits.cpp
+++ InterviewBit/ReverseBits.cpp
@@ -1,26 +1,9 @@
-unsigned int Solution::reverse(unsigned int n) {
-    unsigned int ans = 0;
-    for(int i = 0; i < 32; i++) {
-        if(n & (1 << i))
-            ans |= (1 << (31 - i));
+unsigned int Solution::reverse(unsigned int A) {
+    unsigned int ret = 0;
+    for(int i = 0; i < 31; i++) {
+        if(A & (1 << i))
+            ret += (1 << (32 - i));
     }
-    return ans;
+    return ret;
 }
-
-/*unsigned int Solution::reverse(unsigned int n) {
-    unsigned int ans = 0;
-    int cnt = 32;
-    while(n) {
-        // left shift(multiply by 2)
-        ans <<= 1;
-        if(n & 1)
-            ans ^= 1;
-        // right shift(divide by 2)    
-        n >>= 1;
-        cnt--;
-    }
-    ans <<= cnt;
-    return ans;
-}*/
-
 
