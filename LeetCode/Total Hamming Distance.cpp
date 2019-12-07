cc8fb1ece997c4fc7ec2e6109138b1d869b81270
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-12-07 17:56:18 +0530

    easy

---
+++ LeetCode/Total Hamming Distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for(int n : nums) {
+                if(n & (1 << i))
+                    cnt++;
+            }
+            ans += cnt * (nums.size() - cnt);
+        }
+        return ans;
+    }
+};
